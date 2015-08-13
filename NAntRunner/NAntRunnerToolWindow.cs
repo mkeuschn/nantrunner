0661dbb9b58d6e3686b6c20af64a5920bfccd1d9
mkeuschn <marko.keuschnig@gmail.com>
2015-08-13 09:04:44 +0200

    Initial Commit

---
+++ NAntRunner/NAntRunnerToolWindow.cs
@@ -0,0 +1,41 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="NAntRunnerToolWindow.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace NAntRunner
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+    using Resources;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("d1262c61-b7b8-44d0-b6e3-27f935db6675")]
+    public class NAntRunnerToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NAntRunnerToolWindow"/> class.
+        /// </summary>
+        public NAntRunnerToolWindow() : base(null)
+        {
+            this.Caption = Common.NAntRunner;
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new NAntRunnerToolWindowControl();
+        }
+    }
+}
---
+++ NAntRunner/NAntRunnerToolWindowControl.xaml.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="NAntRunnerToolWindowControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace NAntRunner
+{
+    using Microsoft.Win32;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for NAntRunnerToolWindowControl.
+    /// </summary>
+    public partial class NAntRunnerToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NAntRunnerToolWindowControl"/> class.
+        /// </summary>
+        public NAntRunnerToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            openFileDialog.DefaultExt = ".build";
+            openFileDialog.Filter = "Build Files (*.build)|*.build|Text Files (*.txt)|*.txt";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = openFileDialog.FileName;
+
+                // parse the NAnt file
+                var properties = new TreeViewItem();
+                properties.Header = "Properties";
+
+                var target = new TreeViewItem();
+                target.Header = "Target";
+
+                NAntTreeView.Items.Add(properties);
+                NAntTreeView.Items.Add(target);
+            }
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+                "NAntBuilderToolWindow");
+        }
+    }
+}
