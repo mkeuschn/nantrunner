240d082639ac5458ac2ed633e58ad67e18c78fc8
mkeuschn <marko.keuschnig@gmail.com>
2015-08-13 20:38:53 +0200

    Add License Text, implement refresh handler

--- NAntRunner/NAntRunnerToolWindowControl.xaml.cs
+++ NAntRunner/NAntRunnerToolWindowControl.xaml.cs
@@ -1,6 +1,19 @@
 ﻿//------------------------------------------------------------------------------
 // <copyright file="NAntRunnerToolWindowControl.xaml.cs" company="Company">
 //     Copyright (c) Company.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // </copyright>
 //------------------------------------------------------------------------------
 
@@ -61,7 +74,10 @@ namespace NAntRunner
 
         private void Refresh_Click(object sender, RoutedEventArgs e)
         {
-            // TODO Reload Content From the same file
+            if (!string.IsNullOrEmpty(_viewController.Filename))
+            {
+                OnReload(sender, e);
+            }
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
@@ -78,7 +94,6 @@ namespace NAntRunner
 
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
-            // TODO Open Settings Dialog
             SettingsWindow settingsWindow = new SettingsWindow();
             settingsWindow.Show();
         }
